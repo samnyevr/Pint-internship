421df6624b6477b1580b031118bc2bd37de881d7
Samuel Nye <snye@pb-snye-mbp.local>
2022-03-18 12:07:08 -0700

    clean up image format demo and added ranking number display

---
+++ Images/imageFormat/ImageFormat.md
@@ -0,0 +1,22 @@
+# Image Type
+
+Image formats are separated with two major types, **Bitmap/Raster** format or **Vector** format. With each format comes with different compression method and provides different functionalities.
+
+## Choose an image from the drop down menu
+
+After you picked which image to display, it will show the image with each image type. The image type that are not supported will display as it's not supported. It will also display the file size and the comparison of it's file size ranking from the smallest to the largest.
+
+> Note
+>
+> > svg can display photos. However, due to it's nature of being a vector based image type, the conversion from any bitmap image type can't replicate the color data or the animated data from it's original source
+>
+> > Due to different conversion method, some file types may be able to be reduced further without degrading the quality, and it might cause the ranking of the file sizes to be skew in one way or another.
+
+## Most used Image Types
+
+- jpg
+- png
+- webp
+- avif
+- gif
+- svg
--- Images/imageFormat/imageFormat.js
+++ Images/imageFormat/imageFormat.js
@@ -7,20 +7,46 @@ const ELEM = {
   list_avif: document.querySelector("#avif"),
   list_gif: document.querySelector("#gif"),
   list_svg: document.querySelector("#svg"),
-  list_image_jpg: document.querySelector("#jpg img"),
-  list_image_png: document.querySelector("#png img"),
-  list_image_webp: document.querySelector("#webp img"),
-  list_image_avif: document.querySelector("#avif img"),
-  list_image_gif: document.querySelector("#gif img"),
-  list_image_svg: document.querySelector("#svg img"),
+  list_image_jpg: document.querySelector("#jpg source"),
+  list_image_png: document.querySelector("#png source"),
+  list_image_webp: document.querySelector("#webp source"),
+  list_image_avif: document.querySelector("#avif source"),
+  list_image_gif: document.querySelector("#gif source"),
+  list_image_svg: document.querySelector("#svg source"),
   list_header_jpg: document.querySelector("#jpg header"),
   list_header_png: document.querySelector("#png header"),
   list_header_webp: document.querySelector("#webp header"),
   list_header_avif: document.querySelector("#avif header"),
   list_header_gif: document.querySelector("#gif header"),
   list_header_svg: document.querySelector("#svg header"),
+  list_rank_jpg: document.querySelector("#jpg_rank"),
+  list_rank_png: document.querySelector("#png_rank"),
+  list_rank_webp: document.querySelector("#webp_rank"),
+  list_rank_avif: document.querySelector("#avif_rank"),
+  list_rank_gif: document.querySelector("#gif_rank"),
+  list_rank_svg: document.querySelector("#svg_rank"),
   obj: null,
 };
+
+// helper function for grabing the actual file from the json file
+function rank_cal(rank) {
+  switch (rank) {
+    case "1":
+      return ELEM.obj["medals"][0];
+    case "2":
+      return ELEM.obj["medals"][1];
+    case "3":
+      return ELEM.obj["medals"][2];
+    case "4":
+      return ELEM.obj["medals"][3];
+    case "5":
+      return ELEM.obj["medals"][4];
+    case "6":
+      return ELEM.obj["medals"][5];
+    default:
+      break;
+  }
+}
 
 // binding different event listeners
 function bindEventListeners() {
@@ -33,72 +59,64 @@ function bindEventListeners() {
   ELEM.select.addEventListener("change", (event) => {
     if (event.target.value in ELEM.obj) {
       ELEM.obj[event.target.value].forEach((key) => {
-        // decision to display image format
-        if (key.display) {
-          switch (key.format) {
-            case "jpg":
-              ELEM.list_jpg.removeAttribute("hidden");
-              ELEM.list_image_jpg.setAttribute("src", key.src);
-              ELEM.list_image_jpg.setAttribute("alt", key.alt);
-              ELEM.list_header_jpg.innerHTML = `<h2>${key.format}</h2><p>${key.fileSize}</p><span>${key.rank}</span>`;
-              break;
-            case "png":
-              ELEM.list_png.removeAttribute("hidden");
-              ELEM.list_image_png.setAttribute("src", key.src);
-              ELEM.list_image_png.setAttribute("alt", key.alt);
-              ELEM.list_header_png.innerHTML = `<h2>${key.format}</h2><p>${key.fileSize}</p><span>${key.rank}</span>`;
-              break;
-            case "webp":
-              ELEM.list_webp.removeAttribute("hidden");
-              ELEM.list_image_webp.setAttribute("src", key.src);
-              ELEM.list_image_webp.setAttribute("alt", key.alt);
-              ELEM.list_header_webp.innerHTML = `<h2>${key.format}</h2><p>${key.fileSize}</p><span>${key.rank}</span>`;
-              break;
-            case "avif":
-              ELEM.list_avif.removeAttribute("hidden");
-              ELEM.list_image_avif.setAttribute("src", key.src);
-              ELEM.list_image_avif.setAttribute("alt", key.alt);
-              ELEM.list_header_avif.innerHTML = `<h2>${key.format}</h2><p>${key.fileSize}</p><span>${key.rank}</span>`;
-              break;
-            case "gif":
-              ELEM.list_gif.removeAttribute("hidden");
-              ELEM.list_image_gif.setAttribute("src", key.src);
-              ELEM.list_image_gif.setAttribute("alt", key.alt);
-              ELEM.list_header_gif.innerHTML = `<h2>${key.format}</h2><p>${key.fileSize}</p><span>${key.rank}</span>`;
-              break;
-            case "svg":
-              ELEM.list_svg.removeAttribute("hidden");
-              ELEM.list_image_svg.setAttribute("src", key.src);
-              ELEM.list_image_svg.setAttribute("alt", key.alt);
-              ELEM.list_header_svg.innerHTML = `<h2>${key.format}</h2><p>${key.fileSize}</p><span>${key.rank}</span>`;
-              break;
-            default:
-              break;
-          }
-        } else {
-          // hiding the element
-          switch (key.format) {
-            case "jpg":
-              ELEM.list_jpg.setAttribute("hidden", "");
-              break;
-            case "png":
-              ELEM.list_png.setAttribute("hidden", "");
-              break;
-            case "webp":
-              ELEM.list_webp.setAttribute("hidden", "");
-              break;
-            case "avif":
-              ELEM.list_avif.setAttribute("hidden", "");
-              break;
-            case "gif":
-              ELEM.list_gif.setAttribute("hidden", "");
-              break;
-            case "svg":
-              ELEM.list_svg.setAttribute("hidden", "");
-              break;
-            default:
-              break;
-          }
+        switch (key.format) {
+          case "jpg":
+            ELEM.list_image_jpg.setAttribute("srcset", key.src);
+            ELEM.list_image_jpg.setAttribute("alt", key.alt);
+            ELEM.list_header_jpg.innerHTML = `<h2>${key.format}</h2><p>${key.fileSize}</p>`;
+            ELEM.list_rank_jpg.setAttribute("src", `${rank_cal(key.rank).src}`);
+            ELEM.list_rank_jpg.setAttribute("alt", `${rank_cal(key.rank).alt}`);
+            break;
+          case "png":
+            ELEM.list_image_png.setAttribute("srcset", key.src);
+            ELEM.list_image_png.setAttribute("alt", key.alt);
+            ELEM.list_header_png.innerHTML = `<h2>${key.format}</h2><p>${key.fileSize}</p>`;
+            console.log(rank_cal(key.rank).src);
+            ELEM.list_rank_png.setAttribute("src", `${rank_cal(key.rank).src}`);
+            ELEM.list_rank_png.setAttribute("alt", `${rank_cal(key.rank).alt}`);
+            break;
+          case "webp":
+            ELEM.list_image_webp.setAttribute("srcset", key.src);
+            ELEM.list_image_webp.setAttribute("alt", key.alt);
+            ELEM.list_header_webp.innerHTML = `<h2>${key.format}</h2><p>${key.fileSize}</p>`;
+            ELEM.list_rank_webp.setAttribute(
+              "src",
+              `${rank_cal(key.rank).src}`
+            );
+            ELEM.list_rank_webp.setAttribute(
+              "alt",
+              `${rank_cal(key.rank).alt}`
+            );
+            break;
+          case "avif":
+            ELEM.list_image_avif.setAttribute("srcset", key.src);
+            ELEM.list_image_avif.setAttribute("alt", key.alt);
+            ELEM.list_header_avif.innerHTML = `<h2>${key.format}</h2><p>${key.fileSize}</p>`;
+            ELEM.list_rank_avif.setAttribute(
+              "src",
+              `${rank_cal(key.rank).src}`
+            );
+            ELEM.list_rank_avif.setAttribute(
+              "alt",
+              `${rank_cal(key.rank).alt}`
+            );
+            break;
+          case "gif":
+            ELEM.list_image_gif.setAttribute("srcset", key.src);
+            ELEM.list_image_gif.setAttribute("alt", key.alt);
+            ELEM.list_header_gif.innerHTML = `<h2>${key.format}</h2><p>${key.fileSize}</p>`;
+            ELEM.list_rank_gif.setAttribute("src", `${rank_cal(key.rank).src}`);
+            ELEM.list_rank_gif.setAttribute("alt", `${rank_cal(key.rank).alt}`);
+            break;
+          case "svg":
+            ELEM.list_image_svg.setAttribute("srcset", key.src);
+            ELEM.list_image_svg.setAttribute("alt", key.alt);
+            ELEM.list_header_svg.innerHTML = `<h2>${key.format}</h2><p>${key.fileSize}</p>`;
+            ELEM.list_rank_svg.setAttribute("src", `${rank_cal(key.rank).src}`);
+            ELEM.list_rank_svg.setAttribute("alt", `${rank_cal(key.rank).alt}`);
+            break;
+          default:
+            break;
         }
       });
     }
