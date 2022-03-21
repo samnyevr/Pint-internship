2422d96632bbf3cb99dd6b4f6a96d628c072f4aa
Samuel Nye <snye@pb-snye-mbp.local>
2022-03-21 08:34:05 -0700

    finalize javascript structture on ImageFormat.js

--- Images/imageFormat/imageFormat.js
+++ Images/imageFormat/imageFormat.js
@@ -16,6 +16,16 @@ function bindQueries() {
     document.querySelectorAll('figure[id|="fig"] .file-type')
   );
 
+  ELEMS.list_fileSize = Array.from(
+    document.querySelectorAll('figure[id|="fig"] .file-size')
+  );
+  ELEMS.list_sources = Array.from(
+    document.querySelectorAll('figure[id|="fig"] .img-source')
+  );
+  ELEMS.list_rank = Array.from(
+    document.querySelectorAll('figure[id|="fig"] .img-rank')
+  );
+
   // Transforming the array into something more easily parsed
   // HTMLElement --> image format {string}: HTMLElement
   // ELEMS.list_fileTypes = ELEMS.list_fileTypes.map((elem) => {
@@ -30,31 +40,39 @@ function bindQueries() {
   });
   ELEMS.list_fileTypes = fileTypes;
 
-  ELEMS.list_fileSize = Array.from(
-    document.querySelectorAll('figure[id|="fig"] .file-size')
-  );
-  ELEMS.list_sources = Array.from(
-    document.querySelectorAll('figure[id|="fig"] .img-source')
-  );
-  ELEMS.list_rank = Array.from(
-    document.querySelectorAll('figure[id|="fig"] .img-rank')
-  );
+  fileTypes = {};
+  ELEMS.list_fileSize.forEach((elem) => {
+    fileTypes[elem.dataset.type] = elem;
+  });
+  ELEMS.list_fileSize = fileTypes;
+
+  fileTypes = {};
+  ELEMS.list_rank.forEach((elem) => {
+    fileTypes[elem.dataset.type] = elem;
+  });
+  ELEMS.list_rank = fileTypes;
+
+  fileTypes = {};
+  ELEMS.list_sources.forEach((elem) => {
+    fileTypes[elem.dataset.type] = elem;
+  });
+  ELEMS.list_sources = fileTypes;
 }
 
 //helper function for setting the different card items their appropriate new contents
 function switchContent({ src, alt, fileSize, rank }, format) {
   ELEMS.list_fileTypes[format].innerHTML = format;
-  ELEMS.list_fileTypes[format].innerHTML = fileSize;
+  ELEMS.list_fileSize[format].innerHTML = fileSize;
 
   // helper function for grabing the actual file from the json file
   function _rankCal(rank) {
     return ELEMS.obj["medals"][Number(rank) - 1];
   }
 
-  // ELEMS.list_sources[format].setAttribute("srcset", src);
-  // ELEMS.list_sources[format].setAttribute("alt", alt);
-  // ELEMS.list_rank[format].setAttribute("src", `${_rankCal(rank).src}`);
-  // ELEMS.list_rank[format].setAttribute("alt", `${_rankCal(rank).alt}`);
+  ELEMS.list_sources[format].setAttribute("srcset", src);
+  ELEMS.list_sources[format].setAttribute("alt", alt);
+  ELEMS.list_rank[format].setAttribute("src", `${_rankCal(rank).src}`);
+  ELEMS.list_rank[format].setAttribute("alt", `${_rankCal(rank).alt}`);
 }
 
 // binding different event listeners
