d7cad96ae738336aa3bd166e26ecd3400477b96a
Samuel Nye <snye@pb-snye-mbp.local>
2022-04-21 16:20:59 -0700

    finished bot code first draft

--- javascript/bots/bot.js
+++ javascript/bots/bot.js
@@ -2,9 +2,10 @@
  * Filename: bot.js
  * Author: Sam Nye
  * Last Date Modified: April 21, 2022
- * Description: This is the scripts for a simple bot to try and breach the security of a form submission
- *
- *
+ * Description: This is the scripts for a simple bot to try and breach the security of a form submission. The bot comes with basic form submission by default, but there arer more functionalities addded to enhance the ability to infitrate the system.
+ * Types of Bot Functionality:
+ * 1) Check for hidden attributes
+ * 2) Imitate mouse movement
  ******************************************/
 
 /*******************
@@ -14,15 +15,20 @@ const ELEMS = {
   allInput: undefined,
   botButton: undefined,
   form: undefined,
+  functionalities: undefined,
 };
 
-/*********************
- * Query Assignments *
- ********************/
-function assignQuery() {
-  ELEMS.allInput = document.querySelectorAll("input");
+/******************************
+ * Global Vaiable Assignments *
+ *****************************/
+function assignGlobal() {
+  ELEMS.allInput = document.querySelectorAll("form  input");
   ELEMS.botButton = document.querySelector("#bot");
   ELEMS.form = document.querySelector("form");
+  ELEMS.functionalities = [
+    document.querySelector("#botEnhanced"),
+    document.querySelector("#botMouse"),
+  ];
 }
 
 /*************************
@@ -30,17 +36,41 @@ function assignQuery() {
  ************************/
 function bindEventListener() {
   ELEMS.botButton.addEventListener("click", () => {
-    ELEMS.allInput.forEach((input) => {
-      if (input.name === "name") {
-        input.value = "John Smith";
+    // implement the basic bot functionality
+    ELEMS.allInput.forEach((element) => {
+      if (element.name === "name") {
+        element.value = "John Smith";
+      } else if (element.type === "number") {
+        element.value = 2;
+      } else if (element.type === "color") {
+        element.value = "#464fbe";
       } else {
-        input.value = "Something";
+        element.value = "Something";
+      }
+    });
+
+    ELEMS.functionalities.forEach((option) => {
+      if (option.checked) {
+        // check for hidden attributes and modify the form submission to avoid honeypot
+        if (option.value === "botEnhanced") {
+          ELEMS.allInput.forEach((element) => {
+            if (element.hidden || element.ariaHidden) {
+              element.value = "";
+            }
+          });
+        }
+        // imitate mouse movement to avoid being detected as a bot
+        else if (option.value === "botMouse") {
+          for (let i = 0; i < 105; i++) {
+            const mouseEvent = new MouseEvent("mousemove");
+            window.dispatchEvent(mouseEvent);
+          }
+        }
       }
     });
 
     const submitEvent = new SubmitEvent("submit");
     ELEMS.form.dispatchEvent(submitEvent);
-    ELEMS.form.submit();
   });
 }
 
@@ -48,7 +78,7 @@ function bindEventListener() {
  * Init Function *
  *****************/
 function init() {
-  assignQuery();
+  assignGlobal();
   bindEventListener();
 }
 
---
+++ javascript/bots/security.js
@@ -0,0 +1,89 @@
+/*******************************************
+ * Filename: antiBot.js
+ * Author: Sam Nye
+ * Last Date Modified: April 21, 2022
+ * Description: This is the scripts for combating the bots for a simple security implementation
+ * Types of Functionality
+ * 1) Blocking simple bot form submission
+ * 2) Detecting mouse movement to determine if it's a bot or not
+ ******************************************/
+
+/*******************
+ * Global Variable *
+ *******************/
+const ELEMS = {
+  form: undefined,
+  trap: undefined,
+  functionalities: undefined,
+  messageBox: undefined,
+  mouseMove: undefined,
+  mouseSuccess: undefined,
+};
+
+/******************************
+ * Global Vaiable Assignments *
+ *****************************/
+function assignGlobal() {
+  ELEMS.form = document.querySelector("form");
+  ELEMS.trap = document.querySelector("#honeyPot");
+  ELEMS.messageBox = document.querySelector("#message");
+  ELEMS.functionalities = [
+    document.querySelector("#basic"),
+    document.querySelector("#mouse"),
+  ];
+  ELEMS.mouseMove = 0;
+  ELEMS.mouseSuccess = false;
+}
+
+/*************************
+ * EventListener Binding *
+ ************************/
+function bindEventListener() {
+  ELEMS.form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    let notBot = true;
+
+    ELEMS.functionalities.forEach((option) => {
+      if (option.checked) {
+        // check if the basic bot detection is turned on
+        if (option.value === "basic" && ELEMS.trap.value !== "") {
+          notBot = false;
+        } else if (option.value === "mouse") {
+          if (!ELEMS.mouseSuccess) notBot = false;
+        }
+      }
+    });
+
+    // display the message after form submission
+    if (notBot) {
+      ELEMS.messageBox.innerHTML = "Successfully Submitted the Form";
+    } else {
+      ELEMS.messageBox.innerHTML = "GET OFF MY LAWN";
+    }
+
+    // reset form for next submission
+    ELEMS.form.reset();
+    ELEMS.mouseMove = 0;
+    ELEMS.mouseSuccess = false;
+  });
+
+  // check if the user has moved their mouse or not
+  window.addEventListener("mousemove", function mouseEventListener(e) {
+    ELEMS.mouseMove++;
+    if (ELEMS.mouseMove > 100) {
+      ELEMS.mouseSuccess = true;
+    }
+  });
+}
+
+/*****************
+ * Init Function *
+ *****************/
+function init() {
+  assignGlobal();
+  bindEventListener();
+}
+
+// initialize after page load
+window.addEventListener("DOMContentLoaded", init);
